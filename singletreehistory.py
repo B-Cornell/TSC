1e60d0ff57a4fb0db670a2705e4fd4ac6c82c399
b-cornell <bhcornell@ucdavis.edu>
2018-08-23 18:07:24 -0700

    Implement binary search to speed up greatly

--- singletreehistory.py
+++ singletreehistory.py
@@ -2,20 +2,39 @@ import numpy as np
 import pandas as pd
 import sys
 
+def binarysearch(somelist, target):
+    first = 0
+    last = len(somelist) - 1
+    found = False
+    while first < last and found == False:
+        midp = (first + last)//2
+        if somelist[midp] == target:
+            found = True
+        elif somelist[midp] > target:
+            last = midp-1
+        else:
+            first = midp+1
+    if found == True:
+        return midp
+    else:
+        return 0
 
 for snapshot in range(10,80):
     print '%2d              ' %snapshot
-    data = pd.read_csv('CSVFiles_X/BigMDPL_6E13_%2d_x.csv' %snapshot)
+    data = pd.read_csv('CSVFilesML/BigMDPL_6E13_%2d_mainleaf.csv' %snapshot)
     data['history'] = pd.Series([] for row in data.iterrows())
     for ss in range(10,snapshot+1):
         sys.stdout.write('%2d\r' %ss)
         sys.stdout.flush()
         ssdata = pd.read_csv('CloseHalos/close_halo_%2d.txt' %ss)
-        for row in data.itertuples():
-            for altrow in ssdata.itertuples():
-                if row.mainLeaf_depthFirstId == altrow.mlida or row.mainLeaf_depthFirstId == altrow.dfida:
-                    row.history.append(ss)
-                    row.history.append(altrow)
+        for row in ssdata.itertuples():
+            mainleafcheck = binarysearch(data.mainLeaf_depthFirstId, row.mlida)
+            if mainleafcheck != 0:
+                data.iloc[mainleafcheck].history.append(row)
+            mainleafcheck = binarysearch(data.mainLeaf_depthFirstId, row.mlidb)
+            if mainleafcheck != 0:
+                data.iloc[mainleafcheck].history.append(row)
+
     data.to_csv('full_tree_history_%2d.csv' %snapshot, index = False)
 
 
